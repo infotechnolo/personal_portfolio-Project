be5b1e99aaa8916abefe226e019e601cabb8afd1
Musta <mustafizibnmujeeb@gmail.com>
2020-05-14 06:02:31 +0400

    My First Commit

---
+++ portfolio/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description= models.CharField(max_length=250)
+    image = models.ImageField(upload_to='media/portfolio/images/')
+    url = models.URLField(blank=True) #this mean url is not a neccassry field, by default blank is fals(hence mandatory)
+
+    
