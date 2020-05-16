7ed5b21adc74b18e167b6a6114e9f5085cd5e8f8
Musta <mustafizibnmujeeb@gmail.com>
2020-05-16 07:24:57 +0400

    with egg bon and test and working

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import all_blog
 
-
 def test(request):
     egg = all_blog.objects.all()
     return render(request, 'blog/all_blogs.html', {'bon':egg})
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Project
 
 def home(request):
-    egg = Project.objects.all()
-    return render(request, 'portfolio/home.html', {'bon':egg})
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects':projects})
