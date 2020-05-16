0396c2af4e11d82082bb87d710f4a70ec4453281
Musta <mustafizibnmujeeb@gmail.com>
2020-05-16 06:59:40 +0400

    with egg bon and test

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import all_blog
 
+
 def test(request):
     egg = all_blog.objects.all()
     return render(request, 'blog/all_blogs.html', {'bon':egg})
